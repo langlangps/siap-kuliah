a86b10592eb56de557623616fe6cc82e0cba093c
langlangps <langlangps0@gmail.com>
2020-05-26 11:44:32 +0700

    menambah fitur home menggunakan bootstrap

--- develop/app/Controllers/Home.php
+++ develop/app/Controllers/Home.php
@@ -1,10 +1,17 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data['user'] = "Langlang Purwasasmita";
+		$data['title'] = "Home | " . $data['user'];
+
+		echo view('templates/header', $data);
+		echo view('home/index', $data);
+		echo view('templates/header');
 	}
 
 	//--------------------------------------------------------------------
